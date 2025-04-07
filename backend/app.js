40ce865843905d5d2d0f342951ffa47333138319
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2025-04-07 17:22:03 +0200

    fix: ajout de l'iimport des routes mqtt

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const deviceRoutes = require('./routes/device');
+const mqttRoutes = require('./routes/mqtt');
 
 const app = express();
 const port = 3000;
