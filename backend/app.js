731b2dc37770003ef0401badd3b372217a17f852
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2025-04-07 17:10:55 +0200

    feat: passage du backend en HTTPS

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,23 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const deviceRoutes = require('./routes/device');
-const mqttRoutes = require('./routes/mqtt')
-
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+
+// Certificats
+const options = {
+  key: fs.readFileSync('/certs/backend.key'),
+  cert: fs.readFileSync('/certs/backend.crt')
+};
 
 mongoose.connect('mongodb://mongo:27017/iothink', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
+});
 
 // Middleware de log
 app.use(morgan('dev'));
@@ -22,6 +28,6 @@ app.use(express.json());
 app.use('/devices', deviceRoutes);
 app.use('/mqtt', mqttRoutes);
 
-app.listen(port, () => {
-  console.log(`🚀 MQTT AUTH backend listening on port ${port}`);
+https.createServer(options, app).listen(port, () => {
+  console.log(`✅ Secure backend running at https://localhost:${port}`);
 });
