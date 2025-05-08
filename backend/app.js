ef7a2bb82ca654e2b9f0b165e7dee7c467d3eed9
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2025-05-08 11:21:11 +0200

    fix: update interval for checking inactive devices from 10 seconds to 5 seconds and adjust inactivity threshold from 1 minute to 15 seconds

--- backend/app.js
+++ backend/app.js
@@ -65,8 +65,8 @@ app.use('/', express.json(), authRoutes);
 
 // Vérifie toutes les minutes
 setInterval(() => {
-  updateInactiveDevices(1); // Inactif après 1 min sans contact
-}, 10 * 1000);
+  updateInactiveDevices(0.25);
+}, 5 * 1000);
 
 https.createServer(options, app).listen(port, () => {
   console.log(`✅ Secure backend running at https://localhost:${port}`);
