c7b0a696a7816fbadc7044aeae325b4fa1a4561f
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2025-05-09 14:06:51 +0200

    fix: remove unused path import in Vite configuration

--- vite-front/vite.config.ts
+++ vite-front/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
-import path from 'path'
 
 const keyPath = '/app/certs/front.key'
 const certPath = '/app/certs/front.crt'
