50f9b4f71bba059ffcd9e5f89d05a800ee569d89
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2025-05-09 14:50:44 +0200

    fix: simplify certs path handling in vite.config.ts

--- vite-front/vite.config.ts
+++ vite-front/vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync(path.resolve(__dirname, './certs/front.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, './certs/front.crt')),
+      key: fs.readFileSync('/certs/front.key'),
+      cert: fs.readFileSync('/certs/front.crt'),
     },
     host: true,       // si tu veux accéder depuis d'autres machines du LAN
     port: 5173,       // ou le port de ton choix
